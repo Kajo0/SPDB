bb78274cec5ee462f4c3fac4609cc76beb606181
Maciej <krysztof1@gmail.com>
2015-01-17 15:01:38 +0100

    Date and time selection added

--- mobile/app/src/main/java/elka/pw/edu/pl/spdb/MainActivity.java
+++ mobile/app/src/main/java/elka/pw/edu/pl/spdb/MainActivity.java
@@ -1,17 +1,32 @@
 package elka.pw.edu.pl.spdb;
 
+import android.app.ActionBar;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Pair;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AutoCompleteTextView;
+import android.widget.CheckBox;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
+import com.google.android.gms.internal.ge;
+
 import org.apache.commons.lang3.StringUtils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -22,6 +37,12 @@ public class MainActivity extends ActionBarActivity {
     private FindRouteTask findRouteTask = null;
     private ProgressDialog progressDialog;
 
+    private SimpleDateFormat dateFormat;
+    private Date date;
+
+    private EditText dateEditView;
+    private CheckBox arrivalTimeCheckBox;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,11 +50,17 @@ public class MainActivity extends ActionBarActivity {
 
         sourceAddressEditText = (AutoCompleteTextView) findViewById(R.id.main_address_source);
         targetAddressEditText = (AutoCompleteTextView) findViewById(R.id.main_address_target);
+        dateEditView = (EditText) findViewById(R.id.dateTimeText);
+        arrivalTimeCheckBox = (CheckBox) findViewById(R.id.arrivalTimeCheckbox);
 
         PlacesAutoCompleteAdapter adapter = new PlacesAutoCompleteAdapter(this, R.layout.list_item, 3);
         sourceAddressEditText.setAdapter(adapter);
         targetAddressEditText.setAdapter(adapter);
 
+        dateFormat = new SimpleDateFormat("d/MM/y k:mm");
+        date = new Date();
+
+        dateEditView.setText(dateFormat.format(date));
         /*sourceAddressEditText.setText("Cyprysowa 9, Warszawa");
         targetAddressEditText.setText("Politechnika, Warszawa");*/
     }
@@ -96,8 +123,8 @@ public class MainActivity extends ActionBarActivity {
 
         @Override
         protected Pair<String, String> doInBackground(final String... params) {
-            return new Pair<String, String>(Util.requestRoute(params[0], params[1], null),
-                    Util.requestTransit(params[0], params[1], null));
+            return new Pair<String, String>(Util.requestRoute(true, params[0], params[1], date.getTime(), arrivalTimeCheckBox.isChecked()),
+                    Util.requestRoute(false, params[0], params[1], date.getTime(), arrivalTimeCheckBox.isChecked()));
         }
 
         @Override
@@ -123,5 +150,40 @@ public class MainActivity extends ActionBarActivity {
             super.onCancelled();
             findRouteTask = null;
         }
+    }
+
+    public void onDateTimeClicked(View view) {
+        LinearLayout layout = new LinearLayout(this);
+        layout.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        layout.setOrientation(LinearLayout.VERTICAL);
+
+        final DatePicker datePicker = new DatePicker(this);
+        datePicker.setCalendarViewShown(false);
+        final TimePicker timePicker = new TimePicker(this);
+        timePicker.setIs24HourView(true);
+
+        layout.addView(datePicker);
+        layout.addView(timePicker);
+
+        datePicker.updateDate(date.getYear() + 1900, date.getMonth(), date.getDate());
+        timePicker.setCurrentHour(date.getHours());
+        timePicker.setCurrentMinute(date.getMinutes());
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(layout);
+        builder.setTitle(getString(R.string.date_time_title))
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        date.setDate(datePicker.getDayOfMonth());
+                        date.setMonth(datePicker.getMonth());
+                        date.setYear(datePicker.getYear() - 1900);
+                        date.setMinutes(timePicker.getCurrentMinute());
+                        date.setHours(timePicker.getCurrentHour());
+                        dateEditView.setText(dateFormat.format(date));
+                    }
+                })
+                .setNegativeButton(android.R.string.cancel, null)
+                .show();
     }
 }
--- mobile/app/src/main/java/elka/pw/edu/pl/spdb/Util.java
+++ mobile/app/src/main/java/elka/pw/edu/pl/spdb/Util.java
@@ -7,31 +7,28 @@ public class Util {
     private static final String PARAM_ORIGIN = "origin";
     private static final String PARAM_DESTINATION = "destination";
     private static final String PARAM_ARRIVAL_TIME = "arrival_time";
+    private static final String PARAM_DEPARTURE_TIME = "departure_time";
 
     private static final String SERVER_MAIN_ADDRESS = "http://mmarkiew.no-ip.info/spdb";
     private static final String SERVLET_ROUTE = SERVER_MAIN_ADDRESS + "/driving/";
     private static final String SERVLET_TRANSTI = SERVER_MAIN_ADDRESS + "/transit/";
 
-    public static String requestRoute(String from, String to, Long arrivalTime) {
+
+    public static String requestRoute(boolean driving, String from, String to, Long time, boolean arrivalTime) {
         Map<String, Object> map = CustomHttpClient.createParameterMap();
         map.put(PARAM_ORIGIN, from);
         map.put(PARAM_DESTINATION, to);
-        if (arrivalTime != null) {
-            map.put(PARAM_ARRIVAL_TIME, arrivalTime);
+        if (time != null) {
+            if (arrivalTime) {
+                map.put(PARAM_ARRIVAL_TIME, time);
+            }
+            else {
+                map.put(PARAM_DEPARTURE_TIME, time);
+            }
         }
-
-        return CustomHttpClient.doGetRequest(SERVLET_ROUTE, map);
-    }
-
-    public static String requestTransit(String from, String to, Long arrivalTime) {
-        Map<String, Object> map = CustomHttpClient.createParameterMap();
-        map.put(PARAM_ORIGIN, from);
-        map.put(PARAM_DESTINATION, to);
-        if (arrivalTime != null) {
-            map.put(PARAM_ARRIVAL_TIME, arrivalTime);
+        if (driving) {
+            return CustomHttpClient.doGetRequest(SERVLET_ROUTE, map);
         }
-
         return CustomHttpClient.doGetRequest(SERVLET_TRANSTI, map);
     }
-
 }
